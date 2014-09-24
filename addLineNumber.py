fe5f384a5c206cc2c8beb92e8c9f9b1310f7a27c
Gamliel <myself@example.com>
2014-09-24 14:10:34 +0100

    A couple of fixes has been added.

--- addLineNumber.py
+++ addLineNumber.py
@@ -2,7 +2,9 @@ if __name__ == "__main__":
 	with open("MongoDevTopic.history", "r") as f:
 		counter = 0;
 		for line in f:
+			lineWithoutEndLine = line[0:(len(line) -1)];
 			if (not (line.startswith('//')) and not(line.startswith('\n'))):
-				lineWithoutEndLine = line[0:(len(line) -1)];
 				print lineWithoutEndLine + '; // #'  + str(counter) + '/';
 				counter += 1;
+			else:
+				print lineWithoutEndLine;
